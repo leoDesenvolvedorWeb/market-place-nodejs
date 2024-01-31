45b5d86cc72f1173d2b01f5797c07b170e7f13c5
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-01-31 08:38:15 -0300

    Fix: arrumando as rotas

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -9,7 +9,7 @@ router.get('/findAll', usuarioController.findAllUsercontroller);
 //rotas post
 router.post('/create', usuarioController.createUserController);
 router.post('/addAddress/:id', usuarioController.addUserAddressController);
-router.post('/addFavProduct/:id', usuarioController.addUserFavProduct);
+router.post('/addFavProduct/:id', usuarioController.addUserFavProductController);
 
 //rotas put
 router.put('/update/:id', usuarioController.updateUserController);
@@ -17,6 +17,6 @@ router.put('/update/:id', usuarioController.updateUserController);
 //rotas delete
 router.delete('/remove/:id', usuarioController.removeUserController);
 router.delete('/removeAddress', usuarioController.removeUserAddressController);
-router.delete('/removeFavProduct', usuarioController.removerUserFavProduct);
+router.delete('/removeFavProduct', usuarioController.removerUserFavProductController);
 
 module.exports = router;
