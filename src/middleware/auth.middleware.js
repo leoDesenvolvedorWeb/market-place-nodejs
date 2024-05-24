209f25388f05756ab69fec9841a778b773ce69bf
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-05-24 06:53:16 -0300

    fix: Instalação do dotenv e implementação

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -20,7 +20,7 @@ module.exports = async (req, res, next) => {
         return res.status(401).send({ message: "Token malformado!"});
     }
 
-    jwt.verify(token, "lkjhgfdSAQWErtyuyioipplplplplp90888888453tetetelelelel", async (err, decoded) => {
+    jwt.verify(token, process.env.SECRET, async (err, decoded) => {
         if(err){
             return res.status(500).send({ message: "Token invalido"});
         }
