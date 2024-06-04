6bd7c8fb68057609482bb8b9aae82f0c973a1607
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2024-06-04 11:42:58 -0300

    Feat: CRUD completo do carrinho de compras

---
+++ src/controller/carrinho.controller.js
@@ -0,0 +1,65 @@
+const CarrinhoService = require("../service/carrinho.service");
+
+const findCarrinhoByIdController = async (req, res) => {
+    try{
+        res.status(200).send(await CarrinhoService.findCarrinhoByIdService(req.params.id));
+    }catch(err){
+        res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+        console.log(err.message);
+    }
+}
+
+const findAllCarrinhoService = async (req, res) => {
+    try{
+        res.status(200).send(await CarrinhoService.findAllCarrinhoService());
+    }catch(err){
+        res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+        try{
+
+        }catch(err){
+            res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+            console.log(err.message);
+        }
+    }
+}
+
+const createCarrinhoService = async (req, res) => {
+    try{
+        const corpo = {
+           ...req.body,
+            userId: req.userId,
+            createAt: new Date() 
+        }
+        
+        res.status(200).send(await CarrinhoService.createCarrinhoService(req.params.id, corpo));
+    }catch(err){
+        res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+        console.log(err.message);
+    }
+}
+
+const updateCarrinhoService = async (req, res) => {
+    try{
+        res.status(200).send(await CarrinhoService.updateCarrinhoService(req.params.id, req.body));
+    }catch(err){
+        res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+        console.log(err.message);
+    }
+}
+
+const deleteCarrinhoService = async (req, res) => {
+    try{
+        res.status(200).send(await CarrinhoService.deleteCarrinhoService(req.params.id));
+    }catch(err){
+        res.status(500).send({ message: "Erro inesperado, tente novamente!"});
+        console.log(err.message);
+    }
+}
+
+module.exports = {
+    findCarrinhoByIdController,
+    findAllCarrinhoService,
+    createCarrinhoService,
+    updateCarrinhoService,
+    deleteCarrinhoService
+}
